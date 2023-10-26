da7b825f7607b95a1f068ecb64b7dda7d01f2d5c
lucas <lucaspverdugo@gmail.com>
2023-10-26 15:45:19 -0300

    agrego la estructura de los activities

---
+++ app/src/main/java/com/example/parcial_2_am_acn4bv_castro_verdugo/ErrorInicioSesion.java
@@ -0,0 +1,14 @@
+package com.example.parcial_2_am_acn4bv_castro_verdugo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ErrorInicioSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error_inicio_sesion);
+    }
+}
---
+++ app/src/main/java/com/example/parcial_2_am_acn4bv_castro_verdugo/Noticias.java
@@ -0,0 +1,14 @@
+package com.example.parcial_2_am_acn4bv_castro_verdugo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Noticias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noticias);
+    }
+}
