cba023a0af3a5b6b196c1e0a3f132264128518c8
lucas <lucaspverdugo@gmail.com>
2023-10-26 15:31:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/parcial_2_am_acn4bv_castro_verdugo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.parcial_2_am_acn4bv_castro_verdugo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
