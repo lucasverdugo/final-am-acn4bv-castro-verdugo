8e38a5590f17f3e509e90e078ef9e4ec4c867092
lucas <lucaspverdugo@gmail.com>
2023-11-26 21:18:09 -0300

    feat: se agrega la implmentación de fiberbase en mainActivity y elementos toast dinámicos en respuestas de app.

--- app/src/main/java/com/example/parcial_2_am_acn4bv_castro_verdugo/MainActivity.java
+++ app/src/main/java/com/example/parcial_2_am_acn4bv_castro_verdugo/MainActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void irAInicioSesion(View view){
@@ -25,4 +30,18 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Toast t1 = new Toast(this);
+            t1.setText("Ya existe un usuario logueado.");
+            t1.show();
+        }
+        else {
+            Intent intent = new Intent(this, InicioSesion.class);
+            startActivity(intent);
+        }
+    }
 }
